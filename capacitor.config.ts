f37a528eea06f637f2f2aaa6e4db856ff15c1e0d
sasha <sasha.antonov092@gmail.com>
2023-02-17 14:08:02 +0900

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'KnowPrice',
+  webDir: 'www',
+  bundledWebRuntime: false
+};
+
+export default config;
