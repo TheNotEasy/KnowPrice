654b896b241018019d7534678b367a0f1a661a6f
TheNotEasy <sasha.antonov092@gmail.com>
2023-08-26 15:23:46 +0900

    tab2 update

--- src/app/components/cart-item/cart-item.component.ts
+++ src/app/components/cart-item/cart-item.component.ts
@@ -1,5 +1,8 @@
-import { Component, ElementRef, Input, OnInit, ViewChild } from '@angular/core';
-import { CheckboxCustomEvent } from '@ionic/angular';
+import { Component, ElementRef, EventEmitter, Input, OnInit, Output, SimpleChanges, ViewChild } from '@angular/core';
+import { Router } from '@angular/router';
+import { CheckboxCustomEvent, IonCheckbox, IonItemSliding, ModalController } from '@ionic/angular';
+import { BehaviorSubject, Subject } from 'rxjs';
+import { CartService } from 'src/app/cart.service';
 import { GlobalService } from 'src/app/global.service';
 import { ItemData } from 'src/app/target.types';
 
@@ -8,16 +11,46 @@ import { ItemData } from 'src/app/target.types';
   templateUrl: './cart-item.component.html',
   styleUrls: ['./cart-item.component.scss'],
 })
-export class CartItemComponent implements OnInit {
+export class CartItemComponent {
   @Input() item!: ItemData
-  @Input() deleteModeTriggered!: boolean
 
-  constructor(public global: GlobalService) { }
+  private _deleteModeTriggered!: boolean
 
-  ngOnInit() {}
+  get deleteModeTriggered() {
+    return this._deleteModeTriggered
+  }
+  @Input() set deleteModeTriggered(val) {
+    this._deleteModeTriggered = val
+    this.updateCheckbox(val)
+  }
+  @Input() checkedInDeleteMode!: boolean
+
+  @Output() onChange = new EventEmitter()
+
+  @ViewChild('itemSliding') itemSliding!: IonItemSliding
+  @ViewChild(IonCheckbox) checkbox!: IonCheckbox
+
+  onTouchEndAction: (() => void) | undefined
+
+  public checked = new BehaviorSubject<boolean>(false)
+
+  constructor(
+    public global: GlobalService,
+    private router: Router,
+    public cart: CartService,
+    public modalController: ModalController
+  ) {}
 
   onMark(id: number, event: CheckboxCustomEvent) {
+    this.onChange.emit()
+
+    event.stopPropagation()
+    event.preventDefault()
+
+    if (this.deleteModeTriggered) return
+
     if (event.detail.checked) {
+      if (this.global.markedCartList.includes(this.item.id)) return
       this.global.markedCartList.push(id)
     }
     else {
@@ -26,12 +59,24 @@ export class CartItemComponent implements OnInit {
         1
       )
     }
-      
+    
     this.global.commit()
   }
 
-  test(event: any) {
-    console.log(event)
+  async onTouchEnd() {
+    const ratio = await this.itemSliding.getSlidingRatio()
+
+    if (ratio == 1) {
+      this.router.navigate(['items', this.item.id])
+      this.itemSliding.close()
+    }
   }
 
+  updateCheckbox(deleteModeTriggered = this.deleteModeTriggered) {
+    this.checked.next((
+      this.global.markedCartList.includes(this.item.id) && !deleteModeTriggered
+    ) || (
+      this.checkedInDeleteMode && deleteModeTriggered
+    ))
+  }
 }
